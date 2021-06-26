5fa1948a645fb61859a203d8545eba2739a1da34
RD <81473475+nylesor28@users.noreply.github.com>
2021-06-26 19:01:11 -0400

    add logic to determine back ground colors for time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,125 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  var saveButton = document.querySelector(".saveBtn");
+  var currentDay = document.querySelector("#currentDay");
+  var schedule = $(".schedule");
+  var itemsArray = [];
+  var today;
 
+  var getItems = function () {
+    tempItemsArray = JSON.parse(localStorage.getItem("workdaySchedule"));
 
-var saveButton = document.querySelector(".saveBtn");
-var currentDay = document.querySelector("#currentDay");
-var schedule = $(".schedule")
-var itemsArray = [];
-var today;
+    if (!tempItemsArray || tempItemsArray.length === 0) {
+      return;
+    } else {
+      itemsArray = tempItemsArray;
+    }
 
-var getItems = function () {
-  itemsArray = JSON.parse(localStorage.getItem("workdayItem")) || [];
+    //   $(".row").each(function () {
+    //     var id = $(this).attr("id")
+    //     var scheduleItem = itemsArray[id]
+    //     console.log(scheduleItem)
+    //   })
+  };
 
-  itemsArray.forEach(function (taskItem) {
+  var saveItems = function () {
+    localStorage.setItem("workdaySchedule", JSON.stringify(itemsArray));
+  };
+
+  var buildEmptyWorkdayArray = function () {
+    for (var i = 9; i <= 20; i++) {
+      var workdayItem = {
+        militaryTime: i,
+        displayTime: "",
+        taskItem: "",
+      };
+
+      if (i < 12) {
+        workdayItem.displayTime = i + "AM";
+      } else if (i === 12) {
+        workdayItem.displayTime = i + "PM";
+      } else {
+        workdayItem.displayTime = i - 12 + "PM ";
+      }
+
+      itemsArray.push(workdayItem);
+    }
+    console.log("After Build Array: ", itemsArray);
+  };
+
+  // var setBackgroup
+
+  var loadRows = function () {
+    for (var i = 0; i < itemsArray.length; i++) {
+      var rowEl = $("<div>");
+      rowEl.attr("class", "row");
+      rowEl.attr("id", i);
+      var hourEl = $("<div>");
+      hourEl.attr("class", "col-md-1 hour ");
+      hourEl.text(itemsArray[i].displayTime);
+      var textAreaEl = $("<textarea>");
+      textAreaEl.attr("class", "col-md-10 task form-control");
+
+      var btnEl = $("<button>");
+      btnEl.attr("class", "saveBtn col-md-1");
+
+      var iconEl = $("<i>");
+      iconEl.attr("class", "far fa-save");
+
+      btnEl.append(iconEl);
+
+      rowEl.append(hourEl);
+      rowEl.append(textAreaEl);
+      rowEl.append(btnEl);
+      schedule.append(rowEl);
+    }
+  };
+
+  var loadPage = function () {
+    buildEmptyWorkdayArray();
+
+    loadRows();
+    getItems();
+
+    today = moment();
+    currentDay.textContent = today.format("dddd MMMM Do");
+
+    // for(var i = 0; i<itemsArray.length; i++){
     $(".row").each(function () {
-      var divHourValue = $(this).children(".hour").text().trim();
-      var divHourTime = parseInt(divHourValue);
-      if (taskItem.time === divHourValue) {
-        $(this).children(".task").text(taskItem.task);
+      var id = parseInt($(this).attr("id"));
+      var scheduleObj = itemsArray[id];
+      $(this).children(".task").text(scheduleObj.taskItem);
+
+      var rowTime = moment().hour(scheduleObj.militaryTime).minute(0).second(0);
+      // console.log("rowTime: ",rowTime);
+
+      var timeDiff = today.diff(rowTime, "minutes");
+
+      if (timeDiff > 0 && timeDiff < 60) {
+        $(this).children(".task").addClass("present");
+      } else if (today.isAfter(rowTime)) {
+        $(this).children(".task").addClass("past");
+      } else {
+        $(this).children(".task").addClass("future");
       }
     });
-  });
-};
+  };
 
-var saveItems = function () {
-  localStorage.setItem("workdaySchedule", JSON.stringify(itemsArray));
-};
+  loadPage();
 
-var buildEmptyWorkdayArray = function () {
-  for (var i = 9; i <= 17; i++) {
-    var workdayItem = {
-      militaryTime: i,
-      displayTime: "",
-      taskItem: "",
-    };
+  $(".saveBtn").on("click", function (event) {
+    var parentDiv = event.target.parentElement;
+    var id = parseInt(parentDiv.id);
+    console.log("ID", id, "event ", parentDiv);
+    var workdayItem = itemsArray[id];
+    console.log("ArrayItem", itemsArray);
 
-    if (i < 12) {
-      workdayItem.displayTime = i + "AM";
-    } else if (i === 12) {
-      workdayItem.displayTime = i + "PM";
-    } else {
-      workdayItem.displayTime = i - 12 + "PM ";
-    }
+    itemsArray[id].taskItem = $(parentDiv).children(".task").val().trim();
 
-    itemsArray.push(workdayItem);
-  }
-  console.log(itemsArray);
-};
+    //itemsArray[id].taskItem = $(this).children(".task").val().trim();
 
-// var setBackgroup
-
-var loadRows = function(){
-    for(var i=0; i<itemsArray.length; i++){
-        var rowEl = $("<div>")
-            rowEl.attr("class", "row")
-            rowEl.attr("id", i)
-        var hourEl =  $("<div>")
-            hourEl.attr("class", "col-md-1 hour ")
-            hourEl.text(itemsArray[i].displayTime)
-        var textAreaEl = $("<textarea>")
-            textAreaEl.attr("class","col-md-10 task form-control")
-
-        var btnEl = $("<button>")
-            btnEl.attr("class", "saveBtn col-md-1")
-           
-        var iconEl = $("<i>")
-            iconEl.attr("class", "far fa-save")
-
-        btnEl.append(iconEl);
-        
-        rowEl.append(hourEl);
-        rowEl.append(textAreaEl);
-        rowEl.append(btnEl);
-        schedule.append(rowEl);
-    }
-}
-
-var loadPage = function () {
-  buildEmptyWorkdayArray();
-  loadRows()
-
- // getItems();
-  today = moment();
-  currentDay.textContent = today.format("dddd MMMM Do");
-
-  // var timeNow= today.format("LT")
-  // var hourNow = today.hour(hh)
-  // console.log(timeNow, hourNow)
-  // moment("9AM", hh)
-
-  $(".row").each(function () {});
-};
-
-loadPage();
-
-$(".saveBtn").on("click",function(event){
-    console.log(event.target.parentElement.id)
-    var workdayItem = itemsArray[event.target.parentElement.id];
-    workdayItem.taskItem = $(this).siblings(".task").val().trim()
-    console.log(workdayItem)
     saveItems();
+  });
 });
-
 
 // function saveTask() {
 //     console.log("in savetask")
@@ -109,5 +129,18 @@ $(".saveBtn").on("click",function(event){
 // //   saveItems();
 // };
 
+// var getItems = function () {
+//     itemsArray = JSON.parse(localStorage.getItem("workdayItem")) || [];
 
-})
+//     itemsArray.forEach(function (taskItem) {
+//       $(".row").each(function () {
+//         var id = $(this).id
+//         var divHourValue = $(this).children(".hour").text().trim();
+//         var divHourTime = parseInt(divHourValue);
+//         console.log("ID", id, "divHour: ", divHourValue, " divHourTime:  ", divHourTime )
+//         if (taskItem.time === divHourValue) {
+//           $(this).children(".task").text(taskItem.task);
+//         }
+//       });
+//     });
+//   };
